4dd99ba5e04407529bfca4a89337a65d95d01c76
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-06-03 11:55:24 +0600

    L1

---
+++ WebApplication3/WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static ProductsCatalog _catalog = new();
+
+        [HttpGet]
+        public IActionResult Products()
+        {
+            return View(_catalog);
+        }
+
+        [HttpPost]
+        public IActionResult Products([FromForm]ProductModel type)
+        {
+            _catalog.GetProducts.Add(type);
+            return View(_catalog);
+        }
+    }
+}
